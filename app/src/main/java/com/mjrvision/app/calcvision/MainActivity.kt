2a5d115e5fb3b4cd5370ff900a2543e517fe2df6
Gabriel MJr <gmjplayys@gmail.com>
2024-07-07 10:36:34 +0200

    chore: enhanced layout colors

--- app/src/main/java/com/mjrvision/app/calcvision/MainActivity.kt
+++ app/src/main/java/com/mjrvision/app/calcvision/MainActivity.kt
@@ -3,17 +3,18 @@ package com.mjrvision.app.calcvision
 import androidx.activity.enableEdgeToEdge
 import com.mjrvision.app.calcfusion.core.activity.BaseActivity
 import com.mjrvision.app.calcvision.databinding.ActivityMainBinding
+import com.mjrvision.app.calcvision.fragment.CalculatorFragment
 
 class MainActivity : BaseActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun initializeActivity() {
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
     override fun initializeAttributes() {
+        replaceFragmentBy(R.id.fragment_container, CalculatorFragment::class.java)
     }
 
     override fun setListeners() {
--- app/src/main/java/com/mjrvision/app/calcvision/fragment/CalculatorFragment.kt
+++ app/src/main/java/com/mjrvision/app/calcvision/fragment/CalculatorFragment.kt
@@ -2,12 +2,13 @@ package com.mjrvision.app.calcvision.fragment
 
 import android.view.View
 import com.mjrvision.app.calcfusion.core.fragment.BaseFragment
+import com.mjrvision.app.calcvision.R
+import com.mjrvision.app.calcvision.databinding.FragmentCalculatorBinding
 
-class CalculatorFragment: BaseFragment() {
-    private lateinit var binding:
+class CalculatorFragment: BaseFragment(R.layout.fragment_calculator) {
+    private lateinit var binding: FragmentCalculatorBinding
 
     override fun initializeAttributes(view: View) {
-        TODO("Not yet implemented")
+        binding = FragmentCalculatorBinding.bind(view)
     }
-
 }
