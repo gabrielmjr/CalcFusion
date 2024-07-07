39da17374f0328f575c50446e6ab8e15b2155fbf
Gabriel MJr <gmjplayys@gmail.com>
2024-07-07 09:20:29 +0200

    chore: added calculator layout

---
+++ app/src/main/java/com/mjrvision/app/calcvision/fragment/CalculatorFragment.kt
@@ -0,0 +1,13 @@
+package com.mjrvision.app.calcvision.fragment
+
+import android.view.View
+import com.mjrvision.app.calcfusion.core.fragment.BaseFragment
+
+class CalculatorFragment: BaseFragment() {
+    private lateinit var binding:
+
+    override fun initializeAttributes(view: View) {
+        TODO("Not yet implemented")
+    }
+
+}
